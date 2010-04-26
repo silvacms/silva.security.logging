04f007766a7c1145e2398c1c59e2b3c9b53a7ab5
Sylvain Viollon <thefunny@gmail.com>
2010-04-26 14:35:27 +0000

    Base of the extension silva.security.logging.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev'
+
+setup(name='silva.security.logging',
+      version=version,
+      description="Log all Silva actions",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silva cms zope security',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='http://infrae.com/products/silva',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src', exclude=['ez_setup']),
+      namespace_packages=['silva', 'silva.security'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'silva.core.services',
+        ],
+      )
